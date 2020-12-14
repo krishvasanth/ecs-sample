63054283a07e35345c607b88ff060920d39df44a
Radhakrishnan C <radhakrishnan.c@jothisoftware.com>
2020-12-14 14:53:14 +0530

    updated app.js

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:14
 WORKDIR /usr/src/app
 COPY package*.json app.js ./
 RUN npm install
-EXPOSE 8080
+EXPOSE 80
 CMD ["node", "app.js"]
 
--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Server ready'))
+app.listen(80, () => console.log('Server ready'))
 
